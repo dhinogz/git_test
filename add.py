ffa355b36f425ad29f3a465d092f3f887e3bdbf1
David Hinojosa <davidhinojosauni@gmail.com>
2021-02-28 14:35:58 -0600

    Clean add.py

--- add.py
+++ add.py
@@ -1,7 +1,10 @@
 def add(num1, num2):
     return num1 + num2
 
-num1 = int(input())
-num2 = int(input())
+def printAdd(num1, num2):
+    print(str(num1) + " + " + str(num2) + " = " + str(add(num1, num2)))
 
-print(str(num1) + " + " + str(num2) + " = " + str(add(num1, num2)))
+num1 = int(input("Number 1: "))
+num2 = int(input("Number 2: "))
+
+printAdd(num1, num2)
